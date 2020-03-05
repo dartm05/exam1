4dbbf081d463a2c9ac5f14b39c4fab12598171d7
Dartm05 <d.arteaga@uniandes.edu.co>
2020-03-05 13:36:07 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const mongo= require("../database/MongoUtils.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ mongo.getDatabases().then(databases=>{
+   res.render("index", {databases})
+ });
 });
 
 module.exports = router;
